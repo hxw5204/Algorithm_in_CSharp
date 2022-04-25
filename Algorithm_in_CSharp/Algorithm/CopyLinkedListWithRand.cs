a70c5ffb78a37ff42cc7e14794675046ecbe495d
Haosen Wang <Haosen@epic.com>
2022-04-24 21:07:56 -0500

    linkedList

--- Algorithm_in_CSharp/Algorithm/CopyLinkedListWithRand.cs
+++ Algorithm_in_CSharp/Algorithm/CopyLinkedListWithRand.cs
@@ -38,5 +38,55 @@ namespace Algorithm_in_CSharp.Algorithm
             }
             return map.GetValueOrDefault(head);
         }
+
+        /// <summary>
+        /// better space, worse time
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static Node2 copyLinkedListWithRandom2(Node2 head)
+        {
+            if(head == null)
+            {
+                return null;
+            }
+            Node2 cur = head;
+            Node2 next = null;
+
+            // copy node and link to every node
+            // 1 -> 2
+            // 1 -> 1' -> 2
+            while( cur != null)
+            {
+                next = cur.next;
+                cur.next = new Node2(cur.value);
+                cur.next.next = next;
+                cur = next;
+            }
+            cur = head;
+            Node2 curCopy = null;
+
+            // set copy node rand
+            // 1 -> 1' -> 2 -> 2'
+            while(cur != null)
+            {
+                next = cur.next.next;
+                curCopy = cur.next;
+                curCopy.rand = cur.rand != null ? cur.rand.next : null;
+                cur = next;
+            }
+            Node2 res = head.next;
+            cur = head;
+            // split
+            while(cur != null)
+            {
+                next = cur.next.next;
+                curCopy = cur.next;
+                cur.next = next;
+                curCopy.next = next != null ? next.next : null;
+                cur = next;
+            }
+            return res;
+        }
     }
 }
