df173282e9409956f20692a4847ec40bc9259b2a
Haosen Wang <Haosen@epic.com>
2022-04-24 20:23:00 -0500

    linkedList

---
+++ Algorithm_in_CSharp/Algorithm/CopyLinkedListWithRand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_in_CSharp.Algorithm
+{
+    public class Node2
+    {
+        public int value;
+        public Node2 next;
+        public Node2 rand;
+
+        public Node2(int data)
+        {
+            this.value = data;
+        }
+    }
+    internal class CopyLinkedListWithRand
+    {
+        public static Node2 copyLinkedListWithRand(Node2 head) {
+            Dictionary<Node2, Node2> map = new Dictionary<Node2, Node2>();
+            Node2 cur = head;
+            while(cur != null)
+            {
+                map.Add(cur, new Node2(cur.value));
+                cur = cur.next;
+            }
+            cur = head;
+            while(cur != null)
+            {
+                //cur 老
+                // map.get(cur) 新
+                map.GetValueOrDefault(cur).next = map.GetValueOrDefault(cur.next);
+                map.GetValueOrDefault(cur).rand = map.GetValueOrDefault(cur.rand);
+                cur = cur.next;
+            }
+            return map.GetValueOrDefault(head);
+        }
+    }
+}
---
+++ Algorithm_in_CSharp/Algorithm/SmallEqualBigger.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_in_CSharp.Algorithm
+{
+    internal class SmallEqualBigger
+    {
+        public static Node listPatition(Node head, int pivot)
+        {
+            Node sH = null; // small head
+            Node sT = null; // small tail
+            Node eH = null; // equal head
+            Node eT = null; // equal tail
+            Node mH = null; // big head
+            Node mT = null; // big tail
+            Node next = null; // save next node
+
+            // every node distributed to three lists
+            while(head != null)
+            {
+                next = head.next;
+                head.next = null;
+                if(head.value < pivot)
+                {
+                    if(sH == null)
+                    {
+                        sH = head;
+                        sT = head;
+                    }
+                    else
+                    {
+                        sT.next = head;
+                        sT = head;
+                    }
+                }
+                else if (head.value == pivot)
+                {
+                    if(eH == null)
+                    {
+                        eH = head;
+                        eT = head;
+                    }
+                    else
+                    {
+                        eT.next = head;
+                        eT = head;
+                    }
+                }
+                else
+                {
+                    if(mH == null)
+                    {
+                        mH = head;
+                        mT = head;
+                    }
+                    else
+                    {
+                        mT.next = head;
+                        mT = head;
+                    }
+                }
+                head = next;
+            }
+
+            // small and equal reconnect
+            if(sT != null)
+            {
+                sT.next = eH;
+                eT = eT == null ? sT : eT;
+            }
+
+            // 上面的if， 不管跑了没有， et
+            // all reconnect
+            if(eT != null)
+            {
+                eT.next = mH;
+            }
+            return sH != null ? sH : (eH != null ? eT : mH);
+        }
+    }
+}
